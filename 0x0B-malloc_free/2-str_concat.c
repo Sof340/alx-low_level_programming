74806f79088308a26a9151128604a27a9a24ce77
Sof340 <sofianeadrar34@gmail.com>
2023-11-21 15:17:19 -0800

    done i wish

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+
+	char *copy;
+	int size1 = 0;
+	int size2 = 0;
+
+	while (s1 != NULL && s1[size1])
+	{
+		size1++;
+	}
+	while (s2 != NULL && s2[size2])
+	{
+		size2++;
+	}
+	copy = malloc(size1 + size2 + 1);
+	if (!copy)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size1 + size2; i++)
+	{
+		if (i < size1)
+		{
+			copy[i] = s1[i];
+		}
+		else
+		{
+			copy[i] = s2[i - size1];
+		}
+	}
+	return (copy);
+}
+
