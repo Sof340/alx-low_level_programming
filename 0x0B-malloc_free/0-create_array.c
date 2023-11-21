fd27864c2c1fd9073d77029500694d9457603456
Sof340 <sofianeadrar34@gmail.com>
2023-11-21 14:33:48 -0800

    betty style fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,9 @@ unsigned int i = 0;
 char *array;
 array = malloc(sizeof(char) * size);
 if (size == 0 || array == NULL)
-
 	return (NULL) ;
 
-while(i < size)
+while( i < size )
 {
 	array[i] = c;
 	i++;
