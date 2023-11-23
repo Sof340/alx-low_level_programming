c0df1b1ca5dbe4557b5b8850aa2694efeec1378a
Sof340 <sofianeadrar34@gmail.com>
2023-11-23 13:37:11 -0800

    fixed

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/*
+ * *string_nconcat - returns a pointer
+ * @ @s1: first bytes of the memory
+ * @s2: first bytes of the memory
+ * @n: first bytes of the memory
+ * Return: pointer to the resulting string dests
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int j = 0, i = 0 , k = 0;
+	char *s;
+	
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	if ( n >= j)
+		n = j;
+	s = malloc(sizeof(char) * (n + i));
+	if (s == NULL)
+		return (NULL);
+
+	for (; k <  i ; k++)
+	{
+		s[k] = s1[k];
+	}
+	
+	for (; k < n ; k++)
+	{
+		s[k] = s2[k];
+	}
+
+	return (s);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _calloc - function that creates an array of chars,
+ *  and initializes it with a specific char
+ * @nmemb: first bytes of the memory
+ * @size: first bytes of the memory
+ * Return: pointer to the resulting string dests
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+	unsigned int i = 0;
+	if ( nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (!s)
+		return (NULL);
+	return (s);
+}
