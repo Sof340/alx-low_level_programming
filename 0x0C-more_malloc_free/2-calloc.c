74cb2415940250087700bfa784805056d9a6117e
Sof340 <sofianeadrar34@gmail.com>
2023-11-24 04:48:52 -0800

    fix3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,5 @@
+#include <stdlib.h>
 #include "main.h"
-
 /**
  * _calloc - function that creates an array of chars,
  *  and initializes it with a specific char
@@ -7,15 +7,20 @@
  * @size: first bytes of the memory
  * Return: pointer to the resulting string dests
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *s;
+	unsigned int i;
 
-	if ( nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
-	if (!s)
+	if (s == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		s[i] = 0;
+	}
 	return (s);
 }
+
