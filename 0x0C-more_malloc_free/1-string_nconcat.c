406ef0f25571b6a87954175a0d8140606d9d330b
Sof340 <sofianeadrar34@gmail.com>
2023-11-24 04:47:19 -0800

    fix2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /*
- * string_nconcat - returns a pointer
+ * string_nconcat - function that creates an array of chars,
+ *  and initializes it with a specific char
  * @s1: first bytes of the memory
  * @s2: first bytes of the memory
  * @n: first bytes of the memory
@@ -12,15 +13,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int j = 0, i = 0, k = 0, f = 0;
 	char *s;
+
 	while (s1 && s1[i])
 		i++;
-
 	while (s2 && s2[j])
 		j++;
 
 	if (n >= j)
 		n = j;
+
 	s = malloc(sizeof(char) * (n + i + 1));
+	
 	if (s == NULL)
 		return (NULL);
 
@@ -35,6 +38,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		k++;
 		f++;
 	}
+
 	s[k] = '\0';
+	
 	return (s);
 }
