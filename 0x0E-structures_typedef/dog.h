6d012634db37b0ad51a9b63d33d15cd27194e020
Sof340 <sofianeadrar34@gmail.com>
2023-11-27 14:34:21 -0800

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,9 +16,13 @@ typedef struct dog
 	float age;
 	char *owner;
 }dog_t;
+#endif
 
+#ifndef FUNCTION
+#define FUNCTION
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
 
