73a848e1118577dfb9333c5ca1347e3d292e2119
Sof340 <sofianeadrar34@gmail.com>
2023-12-01 02:00:48 -0800

    fix3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,6 +17,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		separator = "";
+	if (n == 0)
+		return;
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
