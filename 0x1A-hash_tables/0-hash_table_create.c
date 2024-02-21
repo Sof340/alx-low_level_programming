af9ca2389f82625967f6cf3c67a1cda7d885d590
Sof340 <sofianeadrar34@gmail.com>
2024-02-21 08:42:54 -0800

    betty error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
+
 /**
  * *hash_table_create - creates a hash table
  * @size: size of the array
  * Return: a hash table struct
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long i;
