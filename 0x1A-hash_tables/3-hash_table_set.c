563732c063cc104cf3d1fe1e2b6e91a456c46d54
Sof340 <sofianeadrar34@gmail.com>
2024-02-21 12:31:06 -0800

    3. >>> ht[betty] = cool

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
 	unsigned long int index;
-
+	
+	if (key == NULL)
+		return 0;
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return 0;
