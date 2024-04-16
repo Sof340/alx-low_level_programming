96117ff07f627bd60b2d47b5a8ca9782b729c32c
Sof340 <sofianeadrar34@gmail.com>
2024-04-16 05:15:30 -0700

    3. >>> ht[betty] = cool

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current, *new;
 
+	if (key[0] == '\0')
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
 	current = ht->array[index];
