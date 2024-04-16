9ae2a1431031ffea524a857cb99d317305a66f82
Sof340 <sofianeadrar34@gmail.com>
2024-04-16 05:36:38 -0700

    3. >>> ht[betty] = cool

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,28 +13,22 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *current, *new;
+	hash_node_t *new;
 
 	if (key[0] == '\0' || ht == NULL || key == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
-	current = ht->array[index];
-	if (current == NULL)
-	{
-		current = malloc(sizeof(hash_node_t));
-		if (current == NULL)
-			return (0);
-		current->key =  strdup(key);
-		current->value = strdup(value);
-		current->next = NULL;
-		return (1);
-	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = current;
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = new;
+	else
+		new->next = ht->array[index];
+
 	return (1);
 }
