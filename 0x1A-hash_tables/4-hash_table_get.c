23b39e54c4e541281afdd2017212e8df06ca0009
Sof340 <sofianeadrar34@gmail.com>
2024-04-17 14:05:35 -0700

    4. >>> ht[betty]

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: pointer to the hash table.
+ * @key: the key to retrieve the value associated with.
+ * Return: the value if succes, NULL if failure.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
